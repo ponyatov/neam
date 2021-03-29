63fd8e5c3d7325fde93238ece08ded4b17cc10cb
Dmitry Ponyatov <dponyatov@gmail.com>
2021-03-29 08:44:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- neam.nimble
+++ neam.nimble
@@ -2,7 +2,7 @@
 
 version       = "0.0.1"
 author        = "Dmitry Ponyatov <dponyatov@gmail.com>"
-description   = "BEAM rework in Nim"
+description   = "Erlang BEAM rework in Nim lang"
 license       = "MIT"
 binDir        = "bin"
 srcDir        = "src"
---
+++ src/metainfo.nim
@@ -0,0 +1,12 @@
+const
+    MODULE*  = "neam"
+    TITLE*   = "Erlang BEAM rework in Nim lang"
+    AUTHOR*  = "Dmitry Ponyatov"
+    EMAIL*   = "dponyatov@gmail.com"
+    YEAR*    = 2020
+    LICENSE* = "MIT"
+    GITHUB*  = "https://github.com/ponyatov/neam"
+
+proc README*(): string = """
+#  `#{MODULE}`
+"""
